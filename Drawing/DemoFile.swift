86719dc58b1081efafa1dc5994b0591bd00fbcb8
shivgirja <amitsrivastava115@yahoo.com>
2021-12-05 18:53:08 +0530

    commit all changes

---
+++ Drawing/DemoFile.swift
@@ -0,0 +1,20 @@
+//
+//  DemoFile.swift
+//  Drawing
+//
+//  Created by Amit Shrivastava on 05/12/21.
+//
+
+import SwiftUI
+
+struct DemoFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DemoFile_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoFile()
+    }
+}
