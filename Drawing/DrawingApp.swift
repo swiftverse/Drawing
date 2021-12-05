4c3f918a98920b6c9805d6b31c353a98fbaf063c
shivgirja <amitsrivastava115@yahoo.com>
2021-12-05 18:02:35 +0530

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Amit Shrivastava on 05/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
